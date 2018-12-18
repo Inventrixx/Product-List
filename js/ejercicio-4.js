1c1dc007c52ba539553e0007f2921eeb2bd7b74b
Your Name <inveniahadouken@gmail.com>
2018-12-17 23:00:43 -0300

    adding coutes

--- js/ejercicio-4.js
+++ js/ejercicio-4.js
@@ -1,9 +1,27 @@
 //variable globaL
 
-var productosGlobal = [];
-var resultado = 0;  
+const productosGlobal = [];
+//subtotal de la compra
+
+let metodoDePago = null;
+
+let resultado = 0;  
+
+let recargoDebito = 1.05;
+
+let recargoCredito = 1.1;
+
+let recargoCheque = 1.20;
+
+let resultadoCredito = 0;
+
+
+
+
+
+
 //probando function on click con botones
-function verDeuda() {
+function comrparProductos() {
   $(".btn-two").on("click", function() {
     $(".waitingText").addClass("visible");
     setTimeout(function(){
@@ -13,20 +31,22 @@ function verDeuda() {
     
   });
 }
-//mi to-do list
 
+//mi to-do list
 function llamandoALaLista() {
+  //let nombreDeUsuario = ("nombreDeUsuario").val();
   var productos = $(".my-todo-list").val();
   var precios = $(".precios").val();
   var miLista = $(".laLista");
     if (productos != 0 && precios != 0) {
-      //creo mi "item list", cada vez que itero
+      //appendeo los items de los inputs a mi div.
     var misProductos = $("<li> " + productos + " " +"$" + precios + "</li>");
     var miBotonDeBorrar = $("<button class='remove-btn'>X</button>");
     misProductos.append(miBotonDeBorrar);
     miLista.append(misProductos);
 
     var misProductosAAgregar = {
+     // userName: nombreDeUsuario,
       nombre: productos,
       precio: precios
     }
@@ -35,16 +55,18 @@ function llamandoALaLista() {
 
     miBotonDeBorrar.on("click", function() {
       $(this).parent().remove(); //borro el item de la lista
-      productosGlobal.splice(n, 1);
+      productosGlobal.splice(n, 1);//lo borro de mi array también
       subtotal();
     });
     $(".my-todo-list").val("");
     $(".precios").val("");
   }
   else {
-    console.log("agregue señora, agregue")
+    console.log("el dinero no importa, pero cómo ashuda")
   }
 }
+
+
 
 //mi función para sumar 
 function subtotal() {
@@ -53,12 +75,31 @@ function subtotal() {
     var productosANumero = parseInt(productosGlobal[i].precio);
     resultado+=productosANumero
   }
-  //si lo agregaba adentro del for, al momento que yo quiero borrar todos mis elementos del arreglo
-  //el for no se ejecutaba porque solo se ejecuta si tiene algo
-  //console.log(resultado)
   $(".subtotal").text("$" + resultado + " | Total de items ingresados: " + productosGlobal.length);
   return resultado;
 }
+
+//el botón de añadir productos
+$(".addProd").on("click", function() {
+  llamandoALaLista();
+  subtotal();
+  $(".continuar").addClass("visible");
+});
+
+
+
+function creandoLosBotones() {
+    var botonEfectivo = $("<button data-mp='ef' class='modal-btn'>Pagar en Efectivo</button>")
+    var botonDebito = $("<button data-mp='deb' class='modal-btn'>Pagar con Debito</button>")
+    var botonCredito = $("<button data-mp='cr' class='modal-btn'>Pagar con Credito</button>")
+    var botonCheque = $("<button data-mp='ch' class='modal-btn'>Pagar con Cheque</button>")
+    var botonVolver = $("<button class='botonVolver'>Volver</button>")
+    $(".modal-button-container").append(botonEfectivo)
+    $(".modal-button-container").append(botonDebito)
+    $(".modal-button-container").append(botonCredito)
+    $(".modal-button-container").append(botonCheque)
+    $(".modal-button-container").append(botonVolver)
+  }
 
 
 function botonContinuar() {
@@ -72,7 +113,6 @@ function botonContinuar() {
     else {
       $(".modal-continuar").addClass("visible");
       $(".precio-final").text("$" + resultado);
-      creandoLosBotones();
       //vuelvo a la compra
       $(".botonVolver").on("click", function(){
         $(".modal-continuar").removeClass("visible");
@@ -81,104 +121,121 @@ function botonContinuar() {
   
 }
 
-function creandoLosBotones() {
-  var cantidadDeBotones = $(".modal-button-container").children().length;
-  //les pongo estos parámetros pa que el botón, una vez que agregue los 4 botones, no agregue más
-  if (productosGlobal.length >= 1 && cantidadDeBotones === 0) {
-    var botonEfectivo = $("<button data-mp='ef' class='modal-btn'>Pagar en Efectivo</button>")
-    var botonDebito = $("<button data-mp='deb' class='modal-btn'>Pagar con Debito</button>")
-    var botonCredito = $("<button data-mp='cr' class='modal-btn'>Pagar con Credito</button>")
-    var botonCheque = $("<button data-mp='ch' class='modal-btn'>Pagar con Cheque</button>")
-    var botonVolver = $("<button class='botonVolver'>Volver</button>")
-    $(".modal-button-container").append(botonEfectivo)
-    $(".modal-button-container").append(botonDebito)
-    $(".modal-button-container").append(botonCredito)
-    $(".modal-button-container").append(botonCheque)
-    $(".modal-button-container").append(botonVolver)
+
+//ordeno los items que compró al final.
+function productosFinales() {
+  for (let i = 0; i < productosGlobal.length; i++){
+   let mostrarCadaProducto = $("<li>" + productosGlobal[i].nombre + "$" + productosGlobal[i].precio + "</li>") 
+   $(".productosFinales").append(mostrarCadaProducto)
   }
-  }
-
-
-//el botón de añadir productos
-$(".addProd").on("click", function() {
-  llamandoALaLista();
-  subtotal();
-  $(".continuar").addClass("visible");
-});
-
-
-//La financiacion. 
-//voy por los cálculos
-/*
-switch(monto) {
-  case efectivo:
-  monto = monto
-  break;
-  case debito: 
-  monto * 1,5 = monto
-  break;
-  case credito: 
-
-
-}
-*/
-verDeuda();
-//creandoLosBotones();
-
-
-
-/*
-hacer un switch();
-//podría poner dos parámetros, el parámetro 1 que sea el medio de pago, y el parámetro 2 el monto
-function garpandoEnEefectivo(efectivo) { //al pe2 pero bue
-  return efectivo
 }
 
-function garpandoEnLaAmex(monto) {
-  //con crédito hay un recargo del 10%
-  var porcentajeAdicional = 1.1;
-  var resultaTRE = monto * porcentajeAdicional;
-  return resultaTRE;
-}
- 
-function garpandoConDebito(monto) {
-  var porcentajeAdicional = 1.15;
-  var resultaDO = monto * porcentajeAdicional;
-  return resultaDO;
+//guardar en LS
+function guardarEnLS() {
+  misProductosAJson = JSON.stringify(productosGlobal);
+  localStorage.setItem("informacionGuardada", misProductosAJson)
 }
 
-function pagandoConChequeGor(monto) {
-  var porcentajeAdicional = 1.2;
-  var resultaCUA = monto * porcentajeAdicional;
-  return resultaCUA;
+//recargar página
+function volverALaPaginaPrincipal() {
+  let botonPaginaPrincipal = $("<button class='recargarPagina'>Pagina Principal</button>");
+  $(".paginaPrincipal").append(botonPaginaPrincipal);
+  $(".recargarPagina").on("click", function() {
+    location.reload();
+  });
+}
+
+function seleccionandoLaCuota() {
+    $(".cuotas").on("click", function(){
+     let lasCuotas =  $(this).val();
+     switch(lasCuotas) {
+       case "1C":
+       $(".description-text").html("En una cuota no hay recargo de interés.")
+       break;
+       case "12C":
+       let recargoCuota12 = resultadoCredito * 1.2;
+       $(".precio-final").html("$" + recargoCuota12);
+       $(".description-text").html("Con 12 cuotas tenés un recargo del 20%");
+       case "24C":
+       let recargoCuota24 = resultadoCredito * 1.45;
+       $(".precio-final").html("$" + recargoCuota24);
+       $(".description-text").html("Con 24 cuotas tenés un recargo del 45%");
+       case "36C":
+       let recargoCuota36 = resultadoCredito * 1.7;
+       $(".precio-final").html("$" + recargoCuota36);
+       $(".description-text").html("Con 36 cuotas tenés un recargo del 70%");
+     }
+    })
+}
+
+function creandoElSelect() {
+  let miSelect = $("<select class='cuotas'></select>");
+  let opcionDefault = $("<option>Elegí la/s cuota/s para continuar</option>")
+  let opcionCuota1 = $("<option value='1C'>1 Cuota</option>")
+  let opcionCuotas12 = $("<option value='12C'>12 Cuotas</option>")
+  let opcionCuotas24 = $("<option value='24C'>24 Cuotas</option>")
+  let opcionCuotas36 = $("<option value='36C'>36 Cuotas</option>")
+
+  miSelect.append(opcionDefault)
+  miSelect.append(opcionCuota1)
+  miSelect.append(opcionCuotas12)
+  miSelect.append(opcionCuotas24)
+  miSelect.append(opcionCuotas36)
+
+  $(".seleccionarCuotas").append(miSelect)
+  
 }
 
 
-var  mediosDePago = [
-  {
-    medio: "credito",
-    recargo: 1.1
-  },
-  {
-    medio: "efectivo",
-    recargo: 1
-  },
-  {
-    medio: "debito",
-    recargo: 1.05
-  },
-  {
-    medio: "cheque",
-    recargo: 1.20
-  }
-]
 
-/*function verRecargo(monto) {
 
+
+function verQueMetodoDePago() {
+  $(".modal-btn").on("click", function() {
+    resultadoDebito =  resultado * recargoDebito;
+    resultadoDebito = resultadoDebito.toFixed(2);
+    resultadoCredito = resultado * recargoCredito;
+    resultadoCredito = resultadoCredito.toFixed(2);
+    resultadoCheque = resultado * recargoCheque;
+    resultadoCheque = resultadoCheque.toFixed(2);
+    let misDatas = $(this).data().mp
+    switch (misDatas) {
+      case "ef":
+      metodoDePago = "efectivo"
+      $(".modal-button-container").addClass("invisible");
+      $(".pregunta-mp").html("Método de pago elegido: " + metodoDePago)
+      productosFinales();
+      volverALaPaginaPrincipal();
+      break;
+      case "deb":
+      metodoDePago = "debito"
+      $(".modal-button-container").addClass("invisible");
+      $(".precio-final").html("$" + resultadoDebito)
+      $(".pregunta-mp").html("Método de pago elegido: " + metodoDePago)
+      productosFinales();
+      volverALaPaginaPrincipal();
+      break;
+      case "ch": 
+      metodoDePago = "cheque"
+      $(".modal-button-container").addClass("invisible");
+      $(".precio-final").html("$" + resultadoCheque)
+      $(".pregunta-mp").html("Método de pago elegido: " + metodoDePago);
+      productosFinales();
+      volverALaPaginaPrincipal();
+      break;
+      case "cr":
+      metodoDePago = "credito"
+      $(".modal-button-container").addClass("invisible");
+      $(".precio-final").html("$" + resultadoCredito)
+      $(".pregunta-mp").html("Método de pago elegido:" + metodoDePago )
+      resultado = resultadoCredito
+      creandoElSelect();
+      seleccionandoLaCuota();
+    }
+  });
 }
 
-/*function garpandoConLaAmex(pasta) {
 
-}
-
-*/
+comrparProductos();
+creandoLosBotones();
+verQueMetodoDePago();
